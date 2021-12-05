35ae46903df80e6357f841eb12fa58265888f69a
Heesung jang <68782077+heesungjang@users.noreply.github.com>
2021-12-05 17:44:09 +0900

    footer 제거

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -2,19 +2,7 @@ import React from 'react';
 import './style.scss';
 
 function PageFooter({ author, githubUrl }) {
-  return (
-    <footer className="page-footer-wrapper">
-      <p className="page-footer">
-        © {new Date().getFullYear()}
-        &nbsp;
-        <a href={githubUrl}>{author}</a>
-        &nbsp;powered by
-        <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
-          &nbsp;zoomkoding-gatsby-blog
-        </a>
-      </p>
-    </footer>
-  );
+  return <footer className="page-footer-wrapper"></footer>;
 }
 
 export default PageFooter;
